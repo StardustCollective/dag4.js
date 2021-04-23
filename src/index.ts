ce0b8ddac9af788474af3be2d4af28dd42c8d9d2
Frank Fox <ffox77@yahoo.com>
2021-04-23 12:51:54 -0700

    add export Dag4Types.DERIVATION_PATH

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,14 @@ import {
   RestApiOptionsRequest as _RestApiOptionsRequest
 } from '@stardust-collective/dag4-core';
 import {dagNetwork, Snapshot as _Snapshot, Transaction as _Transaction} from '@stardust-collective/dag4-network';
-import {keyStore, HDKey as _HDKey} from '@stardust-collective/dag4-keystore';
+import {keyStore, HDKey as _HDKey, DERIVATION_PATH as _DERIVATION_PATH} from '@stardust-collective/dag4-keystore';
 import {PendingTx as _PendingTx, NetworkInfo as _NetworkInfo} from '@stardust-collective/dag4-network/types';
 import {DagAccount, DagMonitor} from '@stardust-collective/dag4-wallet';
 
 
 export namespace Dag4Types {
   export type HDKey = _HDKey;
+  export type DERIVATION_PATH = _DERIVATION_PATH;
   export type RestApi = _RestApi;
   export type IKeyValueDb = _IKeyValueDb;
   export type IHttpClient = _IHttpClient;
