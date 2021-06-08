9dc1c62e0d5b8394350d2066f2239b847b2166a9
Frank Fox <ffox77@yahoo.com>
2021-06-08 13:17:26 -0700

    cleanup comments and extraneous code.

--- packages/dag4-keyring/src/accounts/ecdsa-account.ts
+++ packages/dag4-keyring/src/accounts/ecdsa-account.ts
@@ -1,22 +1,16 @@
 import Wallet from 'ethereumjs-wallet';
 import {Buffer} from 'buffer';
 import * as ethUtil from 'ethereumjs-util';
-import {ECDSASignatureBuffer} from 'ethereumjs-util/dist/signature';
-import * as sigUtil from 'eth-sig-util';
+
 import {
   KeyringAccountSerialized,
   KeyringAccountState,
   KeyringAssetType,
-  KeyringAssetInfo,
   KeyringNetwork
 } from '../kcs';
 import {Asset} from './asset';
 
-//https://coders-errand.com/ecrecover-signature-verification-ethereum/
-// https://arxiv.org/pdf/1508.00184.pdf#:~:text=ECDSA%20offers%20same%20level%20of%20security%20with%20smaller%20key%20sizes.&text=Data%20size%20for%20RSA%20is%20smaller%20than%20ECDSA.&text=ECDSA%20provides%20faster%20computations%20and,is%20much%20shorter%20in%20ECDSA.
 
-// ethereumjs-tx moved
-//https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/tx
 export abstract class EcdsaAccount {
 
   tokens: string[];
