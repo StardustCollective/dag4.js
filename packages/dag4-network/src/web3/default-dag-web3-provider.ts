46ef2a29b74fa559912f2ba92709ddd41f2693ec
Frank Fox <ffox77@gmail.com>
2021-08-25 14:13:07 -0700

    add default dag-provider

---
+++ packages/dag4-network/src/web3/default-dag-web3-provider.ts
@@ -0,0 +1,59 @@
+import {BlockExplorerApi, LoadBalancerApi} from "../api";
+import {NetworkInfo} from "../types";
+import {globalDagNetwork} from "../global-dag-network";
+
+export class DefaultDagWeb3Provider {
+
+    private loadBalancerApi = new LoadBalancerApi();
+    private blockExplorerApi = new BlockExplorerApi();
+    private connectedNetwork: NetworkInfo;
+
+    constructor(netInfo?: NetworkInfo) {
+        if (netInfo) {
+            this.setNetwork(netInfo);
+        }
+    }
+
+    config(netInfo?: NetworkInfo) {
+        if (netInfo) {
+            this.setNetwork(netInfo);
+        }
+        else {
+            return this.connectedNetwork;
+        }
+    }
+
+    setNetwork(netInfo: NetworkInfo) {
+        if (this.connectedNetwork !== netInfo) {
+
+            this.connectedNetwork = netInfo;
+
+            if (!this.blockExplorerApi) {
+                this.loadBalancerApi = new LoadBalancerApi();
+                this.blockExplorerApi = new BlockExplorerApi();
+            }
+
+            this.blockExplorerApi.config().baseUrl(netInfo.beUrl);
+            this.loadBalancerApi.config().baseUrl(netInfo.lbUrl);
+        }
+    }
+
+    async getBalance(address: string) {
+        const balObj = await this.getLoadBalancerApi().getAddressBalance(address);
+        return balObj.balance;
+    }
+
+    async getTransactionCount(address: string) {
+        const balObj = await this.getLoadBalancerApi().getAddressLastAcceptedTransactionRef(address);
+        return balObj.ordinal;
+    }
+
+    private getLoadBalancerApi() {
+        return this.loadBalancerApi || globalDagNetwork.loadBalancerApi;
+    }
+
+    private getBlockExplorerApi() {
+        return this.blockExplorerApi || globalDagNetwork.blockExplorerApi;
+    }
+
+}
