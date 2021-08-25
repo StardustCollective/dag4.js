769de9d4019abb78df7d8144187391c338dbe02d
Frank Fox <ffox77@gmail.com>
2021-08-25 14:36:08 -0700

    fix export

--- packages/dag4-network/src/index.ts
+++ packages/dag4-network/src/index.ts
@@ -1,5 +1,6 @@
 export * from './global-dag-network'
 export * from './dag-network'
+export * from './web3'
 export * from './api'
 export * from './dto'
 export * from './types'
---
+++ packages/dag4-network/src/web3/index.ts
@@ -0,0 +1,1 @@
+export * from './default-dag-web3-provider'
